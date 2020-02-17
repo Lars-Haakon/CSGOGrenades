62bfb80b5778fec7d6031c4eaa6dde6d5afd6c31
lnn <lnn@dips.no>
2020-02-17 13:43:05 +0100

    not able to make fullscreen happen

--- functions.js
+++ functions.js
@@ -1,15 +1,3 @@
-function openFullscreen(elem) {
-  if (elem.requestFullscreen) {
-    elem.requestFullscreen();
-  } else if (elem.mozRequestFullScreen) { /* Firefox */
-    elem.mozRequestFullScreen();
-  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-    elem.webkitRequestFullscreen();
-  } else if (elem.msRequestFullscreen) { /* IE/Edge */
-    elem.msRequestFullscreen();
-  }
-}
-
 function search()
 {
   var videotest = document.getElementById('videotest');

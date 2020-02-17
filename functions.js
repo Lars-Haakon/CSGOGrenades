13d12d0ef98d5c33f5ba5fa78f01f880d0e9b131
lnn <lnn@dips.no>
2020-02-17 13:27:32 +0100

    videotest

--- functions.js
+++ functions.js
@@ -1,3 +1,14 @@
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
 
 function search()
 {

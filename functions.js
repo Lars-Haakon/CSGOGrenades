c1bafc92e46f1be2b45537086544a30c46e48bd8
Martin Eggesvik <martineggesvik@hotmail.com>
2020-02-18 22:07:21 +0100

    Fixed logic for new layout

--- functions.js
+++ functions.js
@@ -4,10 +4,10 @@ function search()
   console.log(videotest);
 
 	var search = {
-		maps:GetFilters("mapdd"), 
-		sides:GetFilters("sidedd"), 
-		sites:GetFilters("sitedd"), 
-		grenades:GetFilters("grenadedd")
+		maps:getMap(),
+		sides:GetFilters("side"),
+		sites:GetFilters("site"),
+		grenades:GetFilters("grenade")
 	};
 	
 	var xhttp = new XMLHttpRequest();
@@ -15,73 +15,34 @@ function search()
 		if (this.readyState == 4 && this.status == 200) {
 		  console.log(this.responseText);
 
-      var imagesBody = document.getElementById('images');
-      while (imagesBody.firstChild) {
-        imagesBody.removeChild(imagesBody.firstChild);
+      var imagesList = document.getElementById('imagesList');
+
+      while (imagesList.firstChild) {
+          imagesList.removeChild(imagesList.firstChild);
       }
 
       var images = JSON.parse(this.responseText);
 
       for(var i = 0; i < images.length; i++)
       {
-        var newDiv = document.createElement("div"); 
-        newDiv.style = "width: 350px;float: left;margin-right: 5px;";
+        var div = document.createElement('div');
+        div.className = "col-md-4";
+        div.innerHTML = "                    <div class=\"card mb-4 box-shadow\">\n" +
+            "                        <img class=\"card-img-top\" src="+images[i].src+">\n" +
+            "                        <div class=\"card-body\">\n" +
+            "                            <p class=\"card-text\">"+"<b>" + images[i].id + "</b>-" +images[i].description+"</p>\n" +
+            "                            <div class=\"d-flex justify-content-between align-items-center\">\n" +
+            "                                <div class=\"btn-group\">\n" +
+            "                                    <button type=\"button\" class=\"btn btn-sm btn-outline-secondary\">View</button>\n" +
+            "                                </div>\n" +
+            "                            </div>\n" +
+            "                        </div>\n" +
+            "                    </div>"
+          imagesList.appendChild(div);
 
-        var img = new Image(); 
-        img.src = images[i].src;
-        img.style = "width: inherit;";
-        newDiv.appendChild(img);
-
-        newDiv.appendChild(document.createElement("br"));
-
-        var newLabel = document.createElement("p");
-        newLabel.innerHTML = "<b>" + images[i].id + "</b>-" + images[i].description;
-        newDiv.appendChild(newLabel);
-
-        imagesBody.appendChild(newDiv);
       }
 		}
 	};
 	xhttp.open("GET", "search.php?search=" + JSON.stringify(search), true);
 	xhttp.send();
 }
-
-    function myDocumentClickHandler(event) {
-
-      var bodyClick = true;
-      element = event.srcElement;
-      while(element != null)
-      {
-        if(element.className == 'dropdown')
-        {
-          bodyClick = false;
-          break;
-        }
-        element = element.parentElement;
-      }
-
-      if(bodyClick) // if document is pressed
-      {
-        var ddfilters = document.getElementsByClassName("dropdown-content");
-
-        var doSearch = false;
-        for(var i = 0; i < ddfilters.length; i++){
-          var first = ddfilters[i].classList.contains("show");
-
-          ddfilters[i].classList.remove("show");
-
-          var second = ddfilters[i].classList.contains("show");
-          if(first && !second)
-          {
-            doSearch = true;
-          }
-        }
-
-        if(doSearch)
-        {
-          search();
-        }
-      }
-    }
-
-    document.onclick = myDocumentClickHandler;
